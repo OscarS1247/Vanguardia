704439c1fc32abfecc6f75a1c127a486db1c29ba
OscarS1247 <82965023+OscarS1247@users.noreply.github.com>
2024-01-29 18:35:27 -0600

    Prueba del Weather API
    Probamos hacer un get del Weather Api usando postman y directamente de nuestra propia api

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  console.log(process.env.VITE_WEATHER_API);
+  res.send('respond with a resources: ' + process.env.VITE_WEATHER_API);
 });
 
 module.exports = router;
