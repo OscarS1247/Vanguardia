359bdb9cc51dce0136ab21b9883e4c64d518996c
OscarS1247 <82965023+OscarS1247@users.noreply.github.com>
2024-02-05 17:31:46 -0600

    Prueba con MongoDB

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+const mongoose = require('mongoose');
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   console.log(process.env.VITE_WEATHER_API);
   res.send('respond with a resources: ' + process.env.VITE_WEATHER_API);
 });
+
+router.get('/db', function (req, res, next) {
+  mongoose.connect(process.env.MONGODB_DATABASE).then(function (response) {
+    res.send("Conectado a la base de datos correctamente")
+  }).catch(function (error) {
+    res.send("Error al conectar con la base de datos")
+  })
+});
+
 
 module.exports = router;
